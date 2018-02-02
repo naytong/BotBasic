05ed76e0d6a908fab83e4d96d3354802d28b613b
Tong <Tong@K17SRVPC0P06K3.kasikornbank.com>
2018-02-02 14:27:05 +0700

    13

--- sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
+++ sample-spring-boot-echo/src/main/java/com/example/bot/spring/echo/EchoApplication.java
@@ -16,70 +16,37 @@
 
 package com.example.bot.spring.echo;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.UncheckedIOException;
-
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.linecorp.bot.client.LineMessagingClient;
-import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.event.Event;
 import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.LocationMessageContent;
-import com.linecorp.bot.model.event.message.StickerMessageContent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.event.source.GroupSource;
-import com.linecorp.bot.model.event.source.RoomSource;
-import com.linecorp.bot.model.event.source.Source;
-import com.linecorp.bot.model.message.LocationMessage;
-import com.linecorp.bot.model.message.Message;
-import com.linecorp.bot.model.message.StickerMessage;
 import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.model.response.BotApiResponse;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
-import lombok.NonNull;
 
 @SpringBootApplication
 @LineMessageHandler
 public class EchoApplication {
-	@Autowired
-    private LineMessagingClient lineMessagingClient;
-
     public static void main(String[] args) {
         SpringApplication.run(EchoApplication.class, args);
     }
 
     @EventMapping
-    public void handleTextMessageEvent(MessageEvent<TextMessageContent> event) throws Exception {
-        TextMessageContent message = event.getMessage();
-        handleTextContent(event.getReplyToken(), event, message);
-    }
-    
-    private void handleTextContent(String replyToken, Event event, TextMessageContent content)
-            throws Exception {
-        String text = content.getText();
-
-        if(text.indexOf("บัญชี")!=-1 || 
-				text.indexOf("บช")!=-1){
+    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+       String result ="";
+		if(event.getMessage().getText().indexOf("บัญชี")!=-1 || 
+				event.getMessage().getText().indexOf("บช")!=-1){
 			StringBuilder str = new StringBuilder();
 			str.append("กสิกรไทย\n" + 
 					"6192018313\n" + 
 					"ธีรวัฒน์ ภาสวงศ์ตระกูล\n" + 
 					"หรือ Prompay (พร้อมเพย์) 0894879738");
-			
-			this.replyText(replyToken, str.toString());
+			result = str.toString();
 		}
 		
-		if(text.indexOf("ไทเท")!=-1){
+		if(event.getMessage().getText().indexOf("ไทเท")!=-1){
 			StringBuilder str = new StringBuilder();
 			str.append("สนใจน็อต size ไหนขนาดเท่าไหร่ครับ ");
 			str.append("M5x10-13-17-20-35 \n" + 
@@ -90,83 +57,13 @@ public class EchoApplication {
 				"M5 เริ่มต้น 140 ครับ ขยับ size ละ 10 บาท\n" + 
 				"M6 เริ่มต้น 150 ครับ ขยับ size ละ 10 บาท\n" + 
 				"M8 30mm 250 35mm 280");
-			this.replyText(replyToken, str.toString());
+			result = str.toString();
 		}
-
-		if(text.equals("RsShop ออกไป")) {
-                Source source = event.getSource();
-                if (source instanceof GroupSource) {
-                    this.replyText(replyToken, "Leaving group");
-                    lineMessagingClient.leaveGroup(((GroupSource) source).getSenderId());
-                } else if (source instanceof RoomSource) {
-                    this.replyText(replyToken, "Leaving room");
-                    lineMessagingClient.leaveRoom(((RoomSource) source).getSenderId());
-                } else {
-                    this.replyText(replyToken, "Bot can't leave from 1:1 chat");
-                }
-             
-        }
+		return new TextMessage(result);
     }
-    
-//    @EventMapping
-//    public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-//	   String text = event.getMessage().getText();
-//	   String replyToken = event.getReplyToken();
-//       String result ="";
-//		
-//
-//		return new TextMessage(result);
-//    }
 
     @EventMapping
     public void handleDefaultMessageEvent(Event event) {
         System.out.println("event: " + event);
-    }
-
-	@EventMapping
-    public void handleStickerMessageEvent(MessageEvent<StickerMessageContent> event) {
-        handleSticker(event.getReplyToken(), event.getMessage());
-    }
-
-    @EventMapping
-    public void handleLocationMessageEvent(MessageEvent<LocationMessageContent> event) {
-        LocationMessageContent locationMessage = event.getMessage();
-        reply(event.getReplyToken(), new LocationMessage(
-                locationMessage.getTitle(),
-                locationMessage.getAddress(),
-                locationMessage.getLatitude(),
-                locationMessage.getLongitude()
-        ));
-    }
-
-	private void handleSticker(String replyToken, StickerMessageContent content) {
-        reply(replyToken, new StickerMessage(
-                content.getPackageId(), content.getStickerId())
-        );
-    }
-
-	private void reply(@NonNull String replyToken, @NonNull Message message) {
-        reply(replyToken, Collections.singletonList(message));
-    }
-
-    private void reply(@NonNull String replyToken, @NonNull List<Message> messages) {
-        try {
-            BotApiResponse apiResponse = lineMessagingClient
-                    .replyMessage(new ReplyMessage(replyToken, messages))
-                    .get();
-          
-        } catch (InterruptedException | ExecutionException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void replyText(@NonNull String replyToken, @NonNull String message) {
-        if (replyToken.isEmpty()) {
-            throw new IllegalArgumentException("replyToken must not be empty");
-        }
-        if (message.length() > 1000) {
-            message = message.substring(0, 1000 - 2) + "……";
-        }
-        this.reply(replyToken, new TextMessage(message));
     }
 }
